3932803ec1f0c88c81cae896b4352650afcc4ad4
mira <75848@siswa.unimas.my>
2022-03-05 20:33:04 +0800

    Initial commit

---
+++ src/main/java/xyz/kannakawaii/learnmod/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package xyz.kannakawaii.learnmod.mixin;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import xyz.kannakawaii.learnmod.LearnMod;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		LearnMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
