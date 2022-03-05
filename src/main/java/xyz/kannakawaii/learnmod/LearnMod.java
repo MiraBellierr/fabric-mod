796f2737ec00bcf23d5a583716005169524e2cfc
mira <75848@siswa.unimas.my>
2022-03-05 21:38:13 +0800

    add mod items

--- src/main/java/xyz/kannakawaii/learnmod/LearnMod.java
+++ src/main/java/xyz/kannakawaii/learnmod/LearnMod.java
@@ -3,12 +3,14 @@ package xyz.kannakawaii.learnmod;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import xyz.kannakawaii.learnmod.item.ModItems;
 
 public class LearnMod implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final String MOD_ID = "learnmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/xyz/kannakawaii/learnmod/item/ModItems.java
@@ -0,0 +1,27 @@
+package xyz.kannakawaii.learnmod.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import xyz.kannakawaii.learnmod.LearnMod;
+
+public class ModItems {
+    public static final Item MYTHRIL_INGOT = registerItem("mythril_ingot",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item MYTHRIL_NUGGET = registerItem("mythril_nugget",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item RAW_MYTHRIL = registerItem("raw_mythril",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(LearnMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        LearnMod.LOGGER.info("Registering Mod Items for " + LearnMod.MOD_ID);
+    }
+}
