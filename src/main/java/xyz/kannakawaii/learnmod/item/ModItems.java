479bb2c3c902a4cc29a9764267b63cdd981c89f2
MiraBellierr <danialzamri0908@gmail.com>
2022-04-06 22:03:18 +0800

    Add dowsing rod item

--- src/main/java/xyz/kannakawaii/learnmod/item/ModItems.java
+++ src/main/java/xyz/kannakawaii/learnmod/item/ModItems.java
@@ -2,20 +2,18 @@ package xyz.kannakawaii.learnmod.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import xyz.kannakawaii.learnmod.LearnMod;
+import xyz.kannakawaii.learnmod.item.custom.DowsingRodItem;
 
 public class ModItems {
 
-    public static final Item MYTHRIL_INGOT = registerItem("mythril_ingot",
-            new Item(new FabricItemSettings().group(ModItemGroups.MYTHRIL)));
-
-    public static final Item MYTHRIL_NUGGET = registerItem("mythril_nugget",
-            new Item(new FabricItemSettings().group(ModItemGroups.MYTHRIL)));
-
-    public static final Item RAW_MYTHRIL = registerItem("raw_mythril",
-            new Item(new FabricItemSettings().group(ModItemGroups.MYTHRIL)));
+    public static final Item MYTHRIL_INGOT = registerItem("mythril_ingot", new Item(new FabricItemSettings().group(ModItemGroups.MYTHRIL)));
+    public static final Item MYTHRIL_NUGGET = registerItem("mythril_nugget", new Item(new FabricItemSettings().group(ModItemGroups.MYTHRIL)));
+    public static final Item RAW_MYTHRIL = registerItem("raw_mythril", new Item(new FabricItemSettings().group(ModItemGroups.MYTHRIL)));
+    public static final Item DOWSING_ROD = registerItem("dowsing_rod", new DowsingRodItem(new FabricItemSettings().group(ItemGroup.MISC).maxDamage(16)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(LearnMod.MOD_ID, name), item);
